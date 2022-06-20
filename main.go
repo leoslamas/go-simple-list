5fee23fec83c3d0d9f7d648beb847a945d77628a
Leonardo Lamas <leoslamas@gmail.com>
2022-06-20 00:24:58 -0300

    update

--- main.go
+++ main.go
@@ -5,13 +5,9 @@ import (
 )
 
 func main() {
-
-	var myList = list.New[int]()
+	var myList = initialize()
 	println("List size: ", myList.Size())
 
-	myList.Add(1)
-	myList.Add(10)
-	myList.Add(100)
 
 	println("List size: ", myList.Size())
 
@@ -27,19 +23,39 @@ func main() {
 	myList.Del(2)
 	printList(myList)
 
-	myList.Pop()
-	printList(myList)
+	myList2 := initialize()
 
+	filtered := list.Filter(myList2, func(x int) bool {
+		return x % 2 == 0
+	})
+	print("Filter even: ")
+	printList(filtered)
+
+	mapped := list.Map(myList2, func(a int) int {
+		return a + 1
+	})
+	print("Map +1: ")
+	printList(mapped)
+
+	reduced := list.Fold(myList2, 0, func(a, b int) int {
+		return a + b
+	})
+	println("Reduce +1: ", reduced)
+}
+
+func initialize() *list.LinkedList[int] {
+	myList := list.New[int]()
+
+	myList.Add(1)
+	myList.Add(10)
+	myList.Add(100)
+
+	return myList
 }
 
 func printList[T any](myList *list.LinkedList[T]) {
 	println("Size: ", myList.Size())
-
 	for val := range myList.Iter() {
 		println("List Iter: ", val)
-	}
-
-	for val := range myList.Iter2() {
-		println("List Iter2: ", val)
 	}
 }
